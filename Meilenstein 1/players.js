e1ee5b1badced9d9c5cf410af09ff0d05af96b87
Chrisgarske <garskechristian@yahoo.de>
2016-05-05 13:35:04 +0200

    added Meilenstein 2 Aufgabe 3

--- Meilenstein 1/players.js
+++ Meilenstein 1/players.js
@@ -1,3 +1,17 @@
-/**
- * Created by christiangarske on 05.05.16.
- */
+function spielerAufrufen() {
+
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '/http://139.59.134.26/', true);
+    xhr.responseType = 'json';
+    xhr.send();
+    var data = xhr.response;
+
+
+
+    }
+
+
+
+}
