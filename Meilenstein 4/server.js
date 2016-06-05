54a6b189b979162c6fbe9464605f9d7e2a6706c0
Rahtainka <rahtainka@t-online.de>
2016-06-05 22:58:34 +0200

    added Meilenstein 4

---
+++ Meilenstein 4/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// main file um eine antwort an den browser zu schicken
+require('./router/main')(app);
+
+// lädt die static files
+app.use(express.static(__dirname + "/views"));
+app.use('/design', express.static(__dirname + '/design'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/javascript', express.static(__dirname + '/javascript'));
+
+var server = app.listen(3000, function () {
+
+    var port = server.address().port
+
+    console.log("Example app listening at http://127.0.0.1:%s", port);
+
+});
