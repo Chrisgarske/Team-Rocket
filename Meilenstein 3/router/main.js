f677f9b31858f7e5a59b396530ac05f087b21389
Rahtainka <rahtainka@t-online.de>
2016-06-05 18:32:57 +0200

    fixed header problem

--- Meilenstein 3/router/main.js
+++ Meilenstein 3/router/main.js
@@ -7,12 +7,12 @@ module.exports = function (app) {
         if (req.query.favorites === 'true') {
             console.log("Got a GET request for the homepage");
             console.log("Nur die Favoriten");
-            res.json(_.where(players, {"favorit": true}));
+            return res.status(200).json(_.where(players, {"favorit": true}));
         }
         else
             console.log("Got a GET request for the homepage");
             console.log("Alle Player");
-            res.json(players);
+            return res.status(200).json(players);
     });
 
 
