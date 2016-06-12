49d02c85c7beedd8f4296c8b88a742d829dc6423
Rahtainka <rahtainka@t-online.de>
2016-06-12 23:53:35 +0200

    error handling for delete and put

--- Meilenstein 4/router/main.js
+++ Meilenstein 4/router/main.js
@@ -1,20 +1,20 @@
 module.exports = function (app) {
 
     const bodyParser = require('body-parser');
-    const players = require('../players.json');
+    let players = require('../players.json');
     app.use(bodyParser.json());
 
     app.get('/api/players', (req, res) => {
 
         let search = req.query.search || 'false';
-        let favorits = req.query.favorites;
+        let favorites = req.query.favorites;
 
-        if (favorits === 'true') {
+        if (favorites === 'true') {
             console.log("Got a GET request for the homepage");
             console.log("Nur die Favoriten");
             return res.status(200).json(players.filter(p => p.favorit));
         }
-        else if (favorits === 'false') {
+        else if (favorites === 'false') {
             console.log("Got a GET request for the homepage");
             console.log("Alle Player");
             return res.status(200).json(players);
@@ -30,7 +30,12 @@ module.exports = function (app) {
 
     app.put('/api/players/:id', (req, res) => {
         console.log('Got a PUT request for Player with ID' + req.params.id);
-        res.status(200).json({"message": "Spieler mit der ID" + req.params.id + "wurde erfolgreich geupdateed"})
+        let filteredPlayers = players.filter(p => p.id !== req.params.id);
+        if(filteredPlayers.length < players.length){
+            res.status(200).json({"message": "Spieler mit der ID" + req.params.id + "wurde erfolgreich geupdated"})
+        }
+        else
+            return res.status(404).json({message: 'Error: 404 Spieler konnte nicht geupdated werden'})
     });
 
     app.post('/api/players', (req, res) => {
@@ -39,12 +44,18 @@ module.exports = function (app) {
             return res.status(200).json({message: 'Spieler wurde erfolgreich gespeichert'})
         }
         else
-            return res.status(404).json({message: 'Error: 404'})
+            return res.status(404).json({message: 'Error: 404 Spieler konnte nicht gespeichert werden'})
     });
 
     app.delete('/api/players/:id', (req, res) => {
         console.log('DELETE request to homepage');
-        return res.status(200).json(players.filter(p => p.id !== req.params.id))
+        let filteredPlayers = players.filter(p => p.id !== req.params.id);
+        if (filteredPlayers.length < players.length){
+            players = filteredPlayers;
+            return res.status(200).json({message: 'Spieler wurde erfolgreich gelöscht'})
+        }
+        else
+            return res.status(404).json({message: 'Error: 404 Spieler konnte nicht gelöscht werden'})
     });
 
 }
